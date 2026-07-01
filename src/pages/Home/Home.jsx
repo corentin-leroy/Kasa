2334c590960d7af3a047c1fd13b44ea400d39a36
Corentin Leroy <corentin.leroyy@gmail.com>
2026-07-01 12:52:07 +0200

    Ajout de la page d'acccueil avec la galerie de logements

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
 import Banner from '../../components/Banner/Banner'
-import banniere from '../../assets/Background.png'
+import Card from '../../components/Card/Card'
+import banniere from '../../assets/background.png'
+import logements from '../../data/logements.json'
+import './Home.scss'
 
 function Home() {
   return (
     <main className="container">
       <Banner image={banniere} title="Chez vous, partout et ailleurs" />
+      <section className="gallery">
+        {logements.map((logement) => (
+          <Card
+            key={logement.id}
+            id={logement.id}
+            cover={logement.cover}
+            title={logement.title}
+          />
+        ))}
+      </section>
     </main>
   )
 }
