c080f51933f548a1fc53ca1997bb0f90162128a0
Corentin Leroy <corentin.leroyy@gmail.com>
2026-07-01 13:41:19 +0200

    Ajout du composant Collapse avec état d'ouverture

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import fleche from '../../assets/Fleche-collapse.svg'
+import './Collapse.scss'
+
+function Collapse({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className="collapse">
+      <div
+        className={`collapse__header ${isOpen ? 'collapse__header--open' : ''}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <h3 className="collapse__title">{title}</h3>
+        <img
+          src={fleche}
+          alt=""
+          className={`collapse__arrow ${isOpen ? 'collapse__arrow--open' : ''}`}
+        />
+      </div>
+      {isOpen && <div className="collapse__content">{children}</div>}
+    </div>
+  )
+}
+
+export default Collapse
