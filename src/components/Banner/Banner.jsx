b411e0ece2c25bdc30429960f995d13309b25fa8
Corentin Leroy <corentin.leroyy@gmail.com>
2026-07-04 13:09:20 +0200

    Ajout du responsive pour le header, footer, page 404 et accueil

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,11 @@ function Banner({ image, title }) {
   return (
     <div className="banner">
       <img src={image} alt="" className="banner__image" />
-      {title && <h1 className="banner__title">{title}</h1>}
+      {title && (
+        <h1 className="banner__title">
+          <span className="banner__title-text">{title}</span>
+        </h1>
+      )}
     </div>
   )
 }
